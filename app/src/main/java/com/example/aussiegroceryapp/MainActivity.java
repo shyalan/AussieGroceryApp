8513c7e226448f0b2f314b439e9d3246ed73f934
shyalan <rshyalan@gmail.com>
2023-04-18 17:56:11 +1000

    -Updated Button Background file -java update for main activity -image/logo upload

--- app/src/main/java/com/example/aussiegroceryapp/MainActivity.java
+++ app/src/main/java/com/example/aussiegroceryapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.aussiegroceryapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startButton = findViewById(R.id.start_button);
+
     }
 }
