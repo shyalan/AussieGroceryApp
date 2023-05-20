07345c078a5420649ab12f5f6f2c340de8604e14
shyalan <rshyalan@gmail.com>
2023-05-20 17:13:17 +0100

    - Bug fixes

--- app/src/main/java/com/example/aussiegroceryapp/AdminProductsActivity.java
+++ app/src/main/java/com/example/aussiegroceryapp/AdminProductsActivity.java
@@ -13,22 +13,21 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.crashlytics.FirebaseCrashlytics;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.DocumentReference;
-
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,6 +48,7 @@ public class AdminProductsActivity extends AppCompatActivity {
         private String name;
         private double price;
         private String storeName;
+        private String imageUrl;
 
         public Product() {
         }
@@ -69,6 +69,14 @@ public class AdminProductsActivity extends AppCompatActivity {
 
         public String getStoreName() {
             return storeName;
+        }
+
+        public void setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+        }
+
+        public String getImageUrl() {
+            return imageUrl;
         }
     }
 
@@ -91,24 +99,6 @@ public class AdminProductsActivity extends AppCompatActivity {
                 openGallery();
             }
         });
-    }
-
-    private void openGallery() {
-        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            mImageUri = data.getData();
-            mUploadButton.setEnabled(false);
-            Toast.makeText(AdminProductsActivity.this, "Image upload successful", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(AdminProductsActivity.this, "Image upload failed", Toast.LENGTH_SHORT).show();
-        }
 
         // Initialize Firestore instance
         db = FirebaseFirestore.getInstance();
@@ -116,7 +106,7 @@ public class AdminProductsActivity extends AppCompatActivity {
         // Initialize Firebase Crashlytics
         FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true);
 
-        // Initialize UI elements
+        // UI
         mLogoImageView = findViewById(R.id.logo_image_view);
         mWelcomeTextView = findViewById(R.id.welcome_text_view);
         mProductNameEditText = findViewById(R.id.product_name_edit_text);
@@ -183,150 +173,110 @@ public class AdminProductsActivity extends AppCompatActivity {
                         // Create a new product object
                         Product product = new Product(name, price, storeName);
 
-                        // Add the product to the Firestore "products" collection
-                        // Query for documents in the "products" collection to check if the product already exists
-                        db.collection("products")
-                                .whereEqualTo("name", name)
-                                .get()
-                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                        if (task.isSuccessful()) {
-                                            boolean productExists = false;
-                                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                                // If a document with the same name exists, set productExists to true and break the loop
-                                                if (document.exists()) {
-                                                    productExists = true;
-                                                    break;
-                                                }
-                                            }
+                        // Check if an image has been selected
+                        if (mImageUri != null) {
+                            // Create a reference to the image in Firebase Storage
+                            StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("product_images/" + ename + ".jpg");
 
-                                            if (productExists) {
-                                                // Product with the same name already exists, show a toast message
-                                                Toast.makeText(AdminProductsActivity.this, "Product with the same name already exists", Toast.LENGTH_SHORT).show();
-                                                mFinishButton.setEnabled(true);
-                                            } else {
-                                                // Add the product to the Firestore "products" collection
-                                                db.collection("products")
-                                                        .add(product)
-                                                        .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-                                                            @Override
-                                                            public void onComplete(@NonNull Task<DocumentReference> task) {
-                                                                if (task.isSuccessful()) {
-                                                                    // Product added successfully, show a toast message
-                                                                    Toast.makeText(AdminProductsActivity.this, "Product added successfully!", Toast.LENGTH_SHORT).show();
-                                                                    mFinishButton.setEnabled(true);
-                                                                } else {
-                                                                    // Product failed to be added, show a toast message with the error
-                                                                    Toast.makeText(AdminProductsActivity.this, "Error adding product: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                                                                    mFinishButton.setEnabled(true);
-                                                                }
-                                                            }
-                                                        });
-                                            }
-                                        } else {
-                                            // Error occurred while retrieving documents, show a toast message with the error
-                                            Toast.makeText(AdminProductsActivity.this, "Error retrieving products: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                                            mFinishButton.setEnabled(true);
+                            // Upload the image to Firebase Storage
+                            storageRef.putFile(mImageUri)
+                                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                        @Override
+                                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                            // Get the download URL of the image
+                                            storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                                @Override
+                                                public void onSuccess(Uri uri) {
+                                                    // Update the product object with the download URL of the image
+                                                    product.setImageUrl(uri.toString());
+
+                                                    // Add the product to Firestore
+                                                    addProductToFirestore(product);
+                                                }
+                                            });
                                         }
-                                    }
-                                });
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            // Enable the button
+                                            mFinishButton.setEnabled(true);
+
+                                            // Show an error message
+                                            Toast.makeText(AdminProductsActivity.this, "Failed to upload image: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                        } else {
+                            // Add the product to Firestore without an image
+                            addProductToFirestore(product);
+                        }
                     } else {
-                        // One or both fields are empty, show a toast message
-                        Toast.makeText(AdminProductsActivity.this, "Please enter both name and price of the product", Toast.LENGTH_SHORT).show();
+                        // Error occurred while retrieving documents, show a toast message with the error
+                        Toast.makeText(AdminProductsActivity.this, "Error retrieving products: Fields are not entered", Toast.LENGTH_SHORT).show();
                         mFinishButton.setEnabled(true);
                     }
                 }
-
-                // Check if an image has been selected
-                if (mImageUri != null) {
-                    // Create a reference to the image in Firebase Storage
-                    StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("product_images/" + ename + ".jpg");
-
-                    // Upload the image to Firebase Storage
-                    storageRef.putFile(mImageUri)
-                            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                                @Override
-                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                                    // Get the download URL of the image
-                                    storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                        @Override
-                                        public void onSuccess(Uri uri) {
-                                            // Create a new document in the "products" collection
-                                            Product product = new Product(ename, Double.parseDouble(priceString), mProductSpinner.getSelectedItem().toString());
-                                            db.collection("products")
-                                                    .add(product)
-                                                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                                                        @Override
-                                                        public void onSuccess(DocumentReference documentReference) {
-                                                            // Update the document with the download URL of the image
-                                                            documentReference.update("imageUrl", uri.toString())
-                                                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                                        @Override
-                                                                        public void onSuccess(Void aVoid) {
-                                                                            // Enable the button
-                                                                            mFinishButton.setEnabled(true);
-
-                                                                            // Show a success message
-                                                                            Toast.makeText(AdminProductsActivity.this, "Product added successfully", Toast.LENGTH_SHORT).show();
-                                                                        }
-                                                                    })
-                                                                    .addOnFailureListener(new OnFailureListener() {
-                                                                        @Override
-                                                                        public void onFailure(@NonNull Exception e) {
-                                                                            // Enable the button
-                                                                            mFinishButton.setEnabled(true);
-
-                                                                            // Show an error message
-                                                                            Toast.makeText(AdminProductsActivity.this, "Error updating product: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                                                                        }
-                                                                    });
-                                                        }
-                                                    })
-                                                    .addOnFailureListener(new OnFailureListener() {
-                                                        @Override
-                                                        public void onFailure(@NonNull Exception e) {
-                                                            // Enable the button
-                                                            mFinishButton.setEnabled(true);
-
-                                                            // Show an error message
-                                                            Toast.makeText(AdminProductsActivity.this, "Error adding product: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                                                        }
-                                                    });
-                                        }
-                                    });
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    // Enable the button
-                                    mFinishButton.setEnabled(true);
-
-                                    // Show an error message
-                                    Toast.makeText(AdminProductsActivity.this, "Error uploading image: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                }
             }
         });
 
-        // Set OnClickListener for "Return to Admin Page" button
+        // Set OnClickListener for "Back" button
         mBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(AdminProductsActivity.this, AdminHomeActivity.class);
-                startActivity(intent);
+                // Navigate back to the previous activity
+                finish();
             }
         });
 
-        // Set OnClickListener for "Add a Store" Button
+        // Set OnClickListener for "Store" button
         mStoreButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Start the StoreActivity
                 Intent intent = new Intent(AdminProductsActivity.this, AdminStoreActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+            mLogoImageView.setImageURI(mImageUri);
+        }
+    }
+
+    private void addProductToFirestore(Product product) {
+        // Add the product to Firestore
+        db.collection("products")
+                .add(product)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        // Enable the button
+                        mFinishButton.setEnabled(true);
+
+                        // Show a success message
+                        Toast.makeText(AdminProductsActivity.this, "Product added successfully", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Enable the button
+                        mFinishButton.setEnabled(true);
+
+                        // Show an error message
+                        Toast.makeText(AdminProductsActivity.this, "Failed to add product: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
